8323947d4a0696e8798e9450b4e758acc92106d3
Sandi Metz <sandi@sandimetz.com>
2018-04-30 10:53:20 -0400

    Phrases class, rename House, rewrite tests

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,4 +1,4 @@
-class House
+class Phrases
   DATA =
     [ "the horse and the hound and the horn that belonged to",
       "the farmer sowing his corn that kept",
@@ -14,12 +14,22 @@ class House
       "the house that Jack built"]
   attr_reader :data
 
-  def initialize
-    @data = DATA
+  def initialize(orderer: UnchangedOrderer.new, list: DATA)
+    @data = orderer.order(list)
+  end
+end
+
+
+class CumulativeTale
+  attr_reader :data, :prefix
+
+  def initialize(phrases: Phrases.new, prefixer: NormalPrefixer.new)
+    @data = phrases.data
+    @prefix = prefixer.prefix
   end
 
   def recite
-    1.upto(12).collect {|i| line(i)}.join("\n")
+    1.upto(data.size).collect {|i| line(i)}.join("\n")
   end
 
   def phrase(num=1)
@@ -29,8 +39,40 @@ class House
   def line(num)
     "#{prefix} #{phrase(num)}.\n"
   end
+end
 
+
+class RandomOrderer
+  def order(data)
+    data.shuffle
+  end
+end
+
+class UnchangedOrderer
+  def order(data)
+    data
+  end
+end
+
+class FixedLastRandomOrderer
+  def order(data)
+    data[0..-2].shuffle << data[-1]
+  end
+end
+
+class NormalPrefixer
   def prefix
     "This is"
   end
 end
+
+class PiratePrefixer
+  def prefix
+    "Thar be"
+  end
+end
+
+puts CumulativeTale.new(
+        phrases: Phrases.new(orderer: FixedLastRandomOrderer.new),
+        prefixer: PiratePrefixer.new).line(12)
+puts
--- house/test/house_test.rb
+++ house/test/house_test.rb
@@ -1,10 +1,20 @@
 require_relative '../../test_helper'
 require_relative '../lib/house'
 
+class PhrasesDouble
+  def data
+    ['1a 1b', '2a 2b', '3a 3b', 'the house that Jack built']
+  end
+end
+
 class HouseTest < Minitest::Test
   attr_reader :tale
   def setup
-    @tale = House.new
+    @tale   = CumulativeTale.new(phrases: PhrasesDouble.new)
+
+    # data    = ['1a 1b', '2a 2b', '3a 3b', 'the house that Jack built']
+    # phrases = Phrases.new(list: data)
+    # @tale   = CumulativeTale.new(phrases: phrases)
   end
 
   def test_line_1
@@ -13,87 +23,77 @@ class HouseTest < Minitest::Test
   end
 
   def test_line_2
-    expected = "This is the malt that lay in the house that Jack built.\n"
+    expected = "This is 3a 3b the house that Jack built.\n"
     assert_equal expected, tale.line(2)
   end
 
   def test_line_3
-    expected = "This is the rat that ate the malt that lay in the house that Jack built.\n"
+    expected = "This is 2a 2b 3a 3b the house that Jack built.\n"
     assert_equal expected, tale.line(3)
   end
 
   def test_line_4
-    expected = "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+    expected = "This is 1a 1b 2a 2b 3a 3b the house that Jack built.\n"
     assert_equal expected, tale.line(4)
-  end
-
-  def test_line_5
-    expected = "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    assert_equal expected, tale.line(5)
-  end
-
-  def test_line_6
-    expected = "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    assert_equal expected, tale.line(6)
-  end
-
-  def test_line_7
-    expected = "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    assert_equal expected, tale.line(7)
-  end
-
-  def test_line_8
-    expected = "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    assert_equal expected, tale.line(8)
-  end
-
-  def test_line_9
-    expected = "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    assert_equal expected, tale.line(9)
-  end
-
-  def test_line_10
-    expected = "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    assert_equal expected, tale.line(10)
-  end
-
-  def test_line_11
-    expected = "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    assert_equal expected, tale.line(11)
-  end
-
-  def test_line_12
-    expected = "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    assert_equal expected, tale.line(12)
   end
 
   def test_all_the_lines
     expected = <<-TEXT
 This is the house that Jack built.
 
-This is the malt that lay in the house that Jack built.
+This is 3a 3b the house that Jack built.
 
-This is the rat that ate the malt that lay in the house that Jack built.
+This is 2a 2b 3a 3b the house that Jack built.
 
-This is the cat that killed the rat that ate the malt that lay in the house that Jack built.
-
-This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-
-This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-
-This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-
-This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-
-This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-
-This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-
-This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-
-This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+This is 1a 1b 2a 2b 3a 3b the house that Jack built.
     TEXT
     assert_equal expected, tale.recite
   end
+end
 
+
+class RandomOrdererTest < Minitest::Test
+  def test_lines
+   Random.srand(1)
+   data     = ['1a 1b', '2a 2b', '3a 3b', 'the house that Jack built']
+   expected = ["the house that Jack built", "3a 3b", "1a 1b", "2a 2b"]
+  #  data     = [['1a', '1b'], ['2a', '2b'], ['3a', '3b'], ['the house', 'that Jack built']]
+  #  expected = [["the house", "that Jack built"], ["3a", "3b"], ["1a", "1b"], ["2a", "2b"]]
+   assert_equal expected, RandomOrderer.new.order(data)
+ end
+end
+
+class FixedLastRandomOrdererTest < Minitest::Test
+  def test_lines
+   Random.srand(1)
+   data     = ['1a 1b', '2a 2b', '3a 3b', 'the house that Jack built']
+   expected = ["1a 1b", "3a 3b", "2a 2b", "the house that Jack built"]
+  #  data     = [['1a', '1b'], ['2a', '2b'], ['3a', '3b'], ['the house', 'that Jack built']]
+  #  expected = [["1a", "1b"], ["3a", "3b"], ["2a", "2b"], ["the house", "that Jack built"]]
+   assert_equal expected, FixedLastRandomOrderer.new.order(data)
+ end
+end
+
+class UnchangedOrdererTest < Minitest::Test
+  def test_lines
+    data     = ['1a 1b', '2a 2b', '3a 3b', 'the house that Jack built']
+    # data     = [['1a', '1b'], ['2a', '2b'], ['3a', '3b'], ['the house', 'that Jack built']]
+    expected = data
+    assert_equal expected, UnchangedOrderer.new.order(data)
+  end
+end
+
+class PhrasesTest < Minitest::Test
+  def test_phrases
+    data     = ['1a 1b', '2a 2b', '3a 3b', 'the house that Jack built']
+    # data     = [['1a', '1b'], ['2a', '2b'], ['3a', '3b'], ['the house', 'that Jack built']]
+    expected = data
+    assert_equal expected, Phrases.new(list: data).data
+  end
+
+  def test_default_phrases
+    data     = ["the horse and the hound and the horn that belonged to", "the farmer sowing his corn that kept", "the rooster that crowed in the morn that woke", "the priest all shaven and shorn that married", "the man all tattered and torn that kissed", "the maiden all forlorn that milked", "the cow with the crumpled horn that tossed", "the dog that worried", "the cat that killed", "the rat that ate", "the malt that lay in", "the house that Jack built"]
+    expected = data
+    assert_equal expected, Phrases.new.data
+  end
 end
